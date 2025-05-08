bb9523910101276dc1c944549f1ba3e8ed5d6ba6
Roberto Mejia <Roberto@zentavos.com>
2025-05-07 21:38:49 -0500

    Move button styling.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input border-gray-800 bg-background text-gray-300 hover:bg-gray-900 hover:hover:bg-ava-darkpurple1 text-gray-50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
