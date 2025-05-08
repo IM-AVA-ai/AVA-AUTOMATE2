1e8d14a3cb176f78929133f36d71c67d4325106d
Roberto Mejia <Roberto@zentavos.com>
2025-05-07 19:31:10 -0500

    Fixed Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // Import getFirestore
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
 
 // Your web app's Firebase configuration
 let analytics;
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBh3HZGNsMwhJ2xnqeBQwIg_EBKLXH_s68",
-  authDomain: "ava-automate2.firebaseapp.com",
-  projectId: "ava-automate2",
-  storageBucket: "ava-automate2.firebasestorage.app",
-  messagingSenderId: "381160095911",
-  appId: "1:381160095911:web:8cc9bdcd3adcee42b4db82",
-  measurementId: "G-0FM6NXN4SB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
